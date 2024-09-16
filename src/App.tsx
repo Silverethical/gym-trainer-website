d236a3368e57d06db91fe2cb19a649c482c36f3f
Mohammad Kargar <kargat504@gmail.com>
2024-09-17 01:40:50 +0330

    Feat: add footer component and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import Footer from "./components/footer/Footer";
+
 function App() {
   return (
-    <main className="container">
-      <h1>Fateme Heidari</h1>
-    </main>
+    <>
+      <main className="container">
+        <h1>Fateme Heidari</h1>
+
+        <Footer />
+      </main>
+    </>
   );
 }
 
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,43 @@
+import "./footer.scss";
+
+const Footer = () => {
+  type footerLinkType = { text: string; link: string };
+
+  const footerLinks: footerLinkType[] = [
+    { text: "سوالات متداول", link: "" },
+    { text: "نظرات کاربران", link: "" },
+    { text: "ارتباط با من", link: "#contact-me" },
+    { text: "خدمات", link: "" },
+    { text: "درباره من", link: "#about-me" },
+  ];
+
+  const socials: { iconUrl: string; link: string }[] = [
+    { iconUrl: "../../../images/socials/phone.svg", link: "tel:" },
+    {
+      iconUrl: "../../../images/socials/telegram.svg",
+      link: "https://t.me/",
+    },
+    {
+      iconUrl: "../../../images/socials/instagram.svg",
+      link: "https://www.instagram.com/",
+    },
+  ];
+  return (
+    <footer id="footer">
+      <div id="links">
+        {footerLinks.map((link) => (
+          <a href={link.link}>{link.text}</a>
+        ))}
+      </div>
+      <div id="social-icons">
+        {socials.map((item) => (
+          <a className="social" href={item.link}>
+            <img src={item.iconUrl} />
+          </a>
+        ))}
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;

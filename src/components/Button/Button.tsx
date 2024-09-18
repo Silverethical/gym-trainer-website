a54efa08625d69c487031e75c250f3f643b6528f
Majid Kargar <majidkargar01@gmail.com>
2024-09-18 18:00:49 +0330

    Feat: Add href attribute

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,37 +1,43 @@
 import "./Button.scss";
 
 type ButtonComponent = {
-	buttonText?: string;
-	startIcon?: string;
-	endIcon?: string;
-	variant?: "filled" | "outlined";
-	size?: "medium" | "large";
-	buttonClass?: string;
-	buttonId?: string;
+    buttonText?: string;
+    startIcon?: string;
+    endIcon?: string;
+    variant?: "filled" | "outlined";
+    size?: "medium" | "large";
+    buttonClass?: string;
+    buttonId?: string;
+    href?: string;
 };
 
 const Button = ({
-	buttonText = "button",
-	startIcon,
-	endIcon,
-	variant = "filled",
-	size = "medium",
-	buttonClass="",
-	buttonId,
+    buttonText = "button",
+    startIcon,
+    endIcon,
+    variant = "filled",
+    size = "medium",
+    buttonClass = "",
+    buttonId,
+    href,
 }: ButtonComponent) => {
-	const buttonClassName = `${buttonClass ?? buttonClass} button-component button-${variant}`;
+    const buttonClassName = `${
+        buttonClass ?? buttonClass
+    } button-component button-${variant}`;
 
-	return (
-		<button
-			id={buttonId && buttonId}
-			data-size={size}
-			className={buttonClassName}
-		>
-			{startIcon && <img className="button-icon" src={startIcon} />}
-			<span>{buttonText}</span>
-			{endIcon && <img className="button-icon" src={endIcon} />}
-		</button>
-	);
+    return (
+        <a href={href ?? href}>
+            <button
+                id={buttonId && buttonId}
+                data-size={size}
+                className={buttonClassName}
+            >
+                {startIcon && <img className="button-icon" src={startIcon} />}
+                <span>{buttonText}</span>
+                {endIcon && <img className="button-icon" src={endIcon} />}
+            </button>
+        </a>
+    );
 };
 
 export default Button;

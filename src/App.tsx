75cd051752cb526457fad66c154e7e75c765c5d4
Majid Kargar <majidkargar01@gmail.com>
2024-09-18 19:42:26 +0330

    Feat: Add about me section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Footer from "./components/footer/Footer";
 import FAQs from "./components/FAQs/FAQs";
 import ContactMe from "./components/ContactMe/ContactMe";
+import AboutMe from "./components/AboutMe/AboutMe";
 
 function App() {
   return (
     <>
       <main className="container">
-        <ContactMe />
+		<ContactMe />
+		<AboutMe/>
         <FAQs />
 
         <Footer />
---
+++ src/components/AboutMe/AboutMe.tsx
@@ -0,0 +1,42 @@
+import "./AboutMe.scss";
+
+const AboutMe = () => {
+	return (
+		<section id="about-me">
+			<h2 className="title">درباره من</h2>
+
+			<div className="about-me-wrapper">
+				<div className="about-me-title-wrapper">
+					<span className="about-me-title">فاطمه حیدری</span>
+					<span className="about-me-title">مربی بدنسازی</span>
+				</div>
+				<div className="about-me-description">
+					لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و
+					با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه
+					و مجله در ستون و سطرآنچنان که لازم است. لورم ایپسوم متن
+					ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از
+					طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون
+					و سطرآنچنان که لازم است.لورم ایپسوم متن ساختگی با تولید
+					سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است
+					چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که
+					لازم است.لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از
+					صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون
+					بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است
+				</div>
+
+				<img
+					className="about-me-circle"
+					id="about-me-red-circle"
+					src="../../../public/images/general/about-me-red-square.png"
+				/>
+				<img
+					id="about-me-yellow-circle"
+					className="about-me-circle"
+					src="../../../public/images/general/about-me-yellow-square.png"
+				/>
+			</div>
+		</section>
+	);
+};
+
+export default AboutMe;

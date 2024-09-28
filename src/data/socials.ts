6227a2e26d90f0e01e6db446da8cd797abfe26c9
Saber Heydari <Silverethical@gmail.com>
2024-09-28 14:27:15 +0330

    Chore: add `contact-me` data

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -2,18 +2,18 @@ type socialType = { iconUrl: string; link: string; text?: string };
 export const socials: socialType[] = [
   {
     iconUrl: "/images/socials/instagram.svg",
-    link: "https://www.instagram.com/",
-    text: "@id",
+    link: "https://www.instagram.com/fa.teme.heydari",
+    text: "@fa.teme.heydari",
   },
 
   {
     iconUrl: "/images/socials/telegram.svg",
-    link: "https://t.me/",
-    text: "@id",
+    link: "https://t.me/Fa_teme_Heydari",
+    text: "@Fa_teme_Heydari",
   },
   {
     iconUrl: "/images/socials/phone.svg",
-    link: "tel:",
-    text: "0912000000",
+    link: "tel:+989335084792",
+    text: "09335084792",
   },
 ];

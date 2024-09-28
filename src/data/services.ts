08651d3d37ee91e64405e673b355d3dae07cccfb
Saber Heydari <Silverethical@gmail.com>
2024-09-28 14:51:36 +0330

    Chore: add images for services

--- src/data/services.ts
+++ src/data/services.ts
@@ -3,7 +3,7 @@ import { ServiceType } from "../types/serviceType";
 export const services: ServiceType[] = [
   {
     serviceTitle: "برنامه تمرین",
-    imageUrl: "/images/services/sampleServicePic.png",
+    imageUrl: "/images/services/training.jpg",
     info: [
       { title: "۵ هفته", price: "300 هزار تومان" },
     ],
@@ -11,14 +11,14 @@ export const services: ServiceType[] = [
   {
     serviceTitle: "برنامه تمرین + تغذیه",
     type: "special-offer",
-    imageUrl: "/images/services/sampleServicePic.png",
+    imageUrl: "/images/services/training_with_diet.jpg",
     info: [
       { title: "۵ هفته", price: "400 هزار تومان" },
     ],
   },
   {
     serviceTitle: "برنامه تغذیه",
-    imageUrl: "/images/services/sampleServicePic.png",
+    imageUrl: "/images/services/diet.jpg",
     info: [
       { title: "۵ هفته", price: "150 هزار تومان" },
     ],

f5d9e07b03f71f7ebcb72b5cd957c33f086f7696
Mohammad Kargar <kargat504@gmail.com>
2024-09-28 14:42:44 +0330

    Feat: add container for services section

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -10,43 +10,45 @@ import "swiper/css/bundle";
 const Services = () => {
   return (
     <section id="services">
-      <h2 className="title">خدمات</h2>
-      <Swiper
-        modules={[Pagination, Autoplay]}
-        slidesPerView={3}
-        pagination={{ clickable: true }}
-        spaceBetween={15}
-        loop={true}
-        autoplay={{
-          delay: 4000,
-          pauseOnMouseEnter: true,
-          disableOnInteraction: false,
-        }}
-        id="services-wrapper"
-        breakpoints={{
-          1040: {
-            slidesPerView: 3,
-          },
-          720: {
-            slidesPerView: 2,
-          },
-          360: {
-            slidesPerView: 1,
-          },
-        }}
-      >
-        {services.map((service, idx) => (
-          <SwiperSlide key={idx}>
-            <ServiceCard
-              info={service.info}
-              serviceTitle={service.serviceTitle}
-              imageUrl={service.imageUrl}
-              type={service.type}
-              button={service.button}
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <div className="container">
+        <h2 className="title">خدمات</h2>
+        <Swiper
+          modules={[Pagination, Autoplay]}
+          slidesPerView={3}
+          pagination={{ clickable: true }}
+          spaceBetween={15}
+          loop={true}
+          autoplay={{
+            delay: 4000,
+            pauseOnMouseEnter: true,
+            disableOnInteraction: false,
+          }}
+          id="services-wrapper"
+          breakpoints={{
+            1040: {
+              slidesPerView: 3,
+            },
+            720: {
+              slidesPerView: 2,
+            },
+            360: {
+              slidesPerView: 1,
+            },
+          }}
+        >
+          {services.map((service, idx) => (
+            <SwiperSlide key={idx}>
+              <ServiceCard
+                info={service.info}
+                serviceTitle={service.serviceTitle}
+                imageUrl={service.imageUrl}
+                type={service.type}
+                button={service.button}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </section>
   );
 };

a033f76d95f273d4c135d6dd54e06a1ad67a084a
Majid Kargar <majidkargar01@gmail.com>
2024-10-03 11:35:18 +0330

    Refactor: change the amount of slides so that it doesnt snapp
    This is the only solution that i came up with rn

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -6,80 +6,79 @@ import { Autoplay } from "swiper/modules";
 
 const loopTextArray: ReactElement[] = [];
 
-for (let i = 0; i < 10; i++) {
-	loopTextArray.push(
-		<SwiperSlide key={i} className="loop-text">
-			FATEMEH HEYDARI
-		</SwiperSlide>
-	);
+for (let i = 0; i < 50; i++) {
+  loopTextArray.push(
+    <SwiperSlide key={i} className="loop-text">
+      FATEMEH HEYDARI
+    </SwiperSlide>,
+  );
 }
 
 const HeroSection = () => {
-	return (
-		<>
-			<section id="hero-section">
-				<div id="hero-section-wrapper" className="container">
-					<div id="hero-section-right-side">
-						<div id="hero-section-text-wrapper">
-							<div className="hero-section-text-wrapper-right">
-								<span className="hero-section-text-primary">
-									آنچه میخواهــی
-								</span>
-								<span className="hero-section-text-secondary">
-									در آیینه ببیـــنی
-								</span>
-							</div>
-							<div className="hero-section-text-primary hero-section-text-wrapper-left">
-								باش
-							</div>
-						</div>
-						<div id="hero-section-button-wrapper">
-							<Button
-								buttonClass="hero-section-buttons"
-								buttonText="تماس با من"
-								variant="filled"
-								href="#contact-me"
-							/>
-							<Button
-								buttonClass="hero-section-buttons"
-								buttonText="مشاهده خدمات"
-								variant="outlined"
-								href="#services"
-							/>
-						</div>
-					</div>
-					<div id="hero-section-left-side">
-						<div id="hero-section-image-wrapper">
-							<img
-								src="/images/general/hero-section-image.png"
-								id="hero-section-image"
-							/>
-							<img
-								src="/images/general/hero-section-image-shadow.png"
-								id="hero-section-image-shadow"
-							/>
-						</div>
-					</div>
-				</div>
-			</section>
-			<Swiper
-				className="infintie-scroll"
-				slidesPerView={8}
-				spaceBetween={18}
-				loop={true}
-				speed={3000}
-				allowTouchMove={false}
-				freeMode={true}
-				modules={[Autoplay]}
-				autoplay={{
-					delay: 0,
-					disableOnInteraction: false,
-				}}
-			>
-				{loopTextArray}
-			</Swiper>
-		</>
-	);
+  return (
+    <>
+      <section id="hero-section">
+        <div id="hero-section-wrapper" className="container">
+          <div id="hero-section-right-side">
+            <div id="hero-section-text-wrapper">
+              <div className="hero-section-text-wrapper-right">
+                <span className="hero-section-text-primary">
+                  آنچه میخواهــی
+                </span>
+                <span className="hero-section-text-secondary">
+                  در آیینه ببیـــنی
+                </span>
+              </div>
+              <div className="hero-section-text-primary hero-section-text-wrapper-left">
+                باش
+              </div>
+            </div>
+            <div id="hero-section-button-wrapper">
+              <Button
+                buttonClass="hero-section-buttons"
+                buttonText="تماس با من"
+                variant="filled"
+                href="#contact-me"
+              />
+              <Button
+                buttonClass="hero-section-buttons"
+                buttonText="مشاهده خدمات"
+                variant="outlined"
+                href="#services"
+              />
+            </div>
+          </div>
+          <div id="hero-section-left-side">
+            <div id="hero-section-image-wrapper">
+              <img
+                src="/images/general/hero-section-image.png"
+                id="hero-section-image"
+              />
+              <img
+                src="/images/general/hero-section-image-shadow.png"
+                id="hero-section-image-shadow"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+      <Swiper
+        className="infintie-scroll"
+        slidesPerView={8}
+        spaceBetween={18}
+        loop={true} // Allow continuous looping
+        speed={4000} // Increase speed for smoother, slower scroll
+        allowTouchMove={false} // Disable manual swiping
+        modules={[Autoplay]}
+        autoplay={{
+          delay: 0, // No delay for continuous scroll
+          disableOnInteraction: false, // Keep scrolling after interactions
+        }}
+      >
+        {loopTextArray}
+      </Swiper>
+    </>
+  );
 };
 
 export default HeroSection;
